2b1587159454ca6d914ca90ddfda6dcca6bb2c5b
Denis Nazmutdinov <w.nazmutdinov@gmail.com>
2018-03-25 20:20:54 +0300

    D3: adding a button

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @root_visit_count = session[:counter].to_i
+    session[:counter] = @root_visit_count + 1
   end
 end
