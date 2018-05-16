db0b41453fcd5620d11b4d162683f1db18acc405
Denis Nazmutdinov <w.nazmutdinov@gmail.com>
2018-05-16 22:37:18 +0500

    I1: adding users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180516153642) do
+ActiveRecord::Schema.define(version: 20180516171015) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -51,6 +51,13 @@ ActiveRecord::Schema.define(version: 20180516153642) do
     t.text "description"
     t.string "image_url"
     t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
