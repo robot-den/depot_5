b99eb9db3f53820fe30b18c36a1f76c9fabd6060
Denis Nazmutdinov <w.nazmutdinov@gmail.com>
2018-05-24 00:07:48 +0500

    J1: selecting the locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authorize
+  before_action :authorize, :set_i18n_locale_from_params
   protect_from_forgery with: :exception
 
   protected
@@ -7,5 +7,15 @@ class ApplicationController < ActionController::Base
   def authorize
     return if session[:user_id] == '0' && User.count.zero?
     redirect_to(login_url, notice: 'Please log in') unless User.find_by(id: session[:user_id])
+  end
+
+  def set_i18n_locale_from_params
+    return unless params[:locale]
+    if I18n.available_locales.map(&:to_s).include?(params[:locale])
+      I18n.locale = params[:locale]
+    else
+      flash.now[:notice] = "#{params[:locale]} translation not available"
+      logger.error flash.now[:notice]
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,16 +7,19 @@ Rails.application.routes.draw do
   end
 
   resources :users
-  resources :orders do
-    post :ship, on: :member
-  end
-
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
 
   resources :products do
     get :who_bought, on: :member
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope '(:locale)' do
+    root 'store#index', as: 'store_index', via: :all
+
+    resources :orders do
+      post :ship, on: :member
+    end
+
+    resources :line_items
+    resources :carts
+  end
 end
